ea0f55bdc14bc3e331d17648bb839954a37a49c2
jason poage <jason.poage91@gmail.com>
2019-05-27 00:00:17 -0600

    modified:   server.js

--- server.js
+++ server.js
@@ -91,10 +91,12 @@ routes.forEach( (route, i) => {
 // Start listening
 const port = process.env.PORT || 5000;
 const sshPort = port + 1;
-//app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => console.log(`Listening on port ${port}`));
+/*
 https.createServer({
 	key: fs.readFileSync('server.key'),
 	cert: fs.readFileSync('server.cert')
 }, app).listen(sshPort, function () {
 	console.log(`Listening on port ${sshPort}`);
 });
+*/
